50f055564978479defe3e892b90e5db38da5658e
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-03-29 20:21:54 -0400

    Added new motors

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from typing import *
 from ctre import *
 import cv2
 import numpy as np
+
 # import RPi.GPIO as gpio
 
 pot_range = 10000
 brake_actuator = TalonSRX(3)
-left_falcon = TalonFX(1)
-right_falcon = TalonFX(2)
+falcon1 = TalonFX(1)
+falcon2 = TalonFX(2)
+falcon3 = TalonFX(3)
+falcon4 = TalonFX(4)
+falcon5 = TalonFX(5)
+falcon6 = TalonFX(6)
 
 
 def main() -> NoReturn:
     while True:
-        right_falcon.setInverted(True)
-        left_falcon.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        right_falcon.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon3.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon4.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon5.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+        falcon6.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+
         brake_actuator.configSelectedFeedbackSensor(FeedbackDevice.Analog, 0)
         text = str(int(get_speed() + 0.5)) + " MPH"
         frame = np.zeros(shape=(1920, 1080, 3))
@@ -23,19 +32,32 @@ def main() -> NoReturn:
         cv2.imshow("Speedometer", frame)
         pot_pos = brake_actuator.getSelectedSensorPosition(0)
         if pot_pos > 10:
-            left_falcon.set(TalonFXControlMode.PercentOutput, pot_pos/pot_range)
-            right_falcon.set(TalonFXControlMode.PercentOutput, pot_pos/pot_range)
+            falcon1.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            falcon2.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            falcon3.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            falcon4.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            falcon5.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            falcon6.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+
             brake_actuator.set(TalonSRXControlMode.PercentOutput, 0)
         elif pot_pos < -10:
-            left_falcon.set(TalonFXControlMode.PercentOutput, 0)
-            right_falcon.set(TalonFXControlMode.PercentOutput, 0)
-            brake_actuator.set(TalonSRXControlMode.PercentOutput, pot_pos/pot_range)
+            falcon1.set(TalonFXControlMode.PercentOutput, 0)
+            falcon2.set(TalonFXControlMode.PercentOutput, 0)
+            falcon3.set(TalonFXControlMode.PercentOutput, 0)
+            falcon4.set(TalonFXControlMode.PercentOutput, 0)
+            falcon5.set(TalonFXControlMode.PercentOutput, 0)
+            falcon6.set(TalonFXControlMode.PercentOutput, 0)
+
+            brake_actuator.set(TalonSRXControlMode.PercentOutput, pot_pos / pot_range)
 
 
 def get_speed() -> float:
-    return (((left_falcon.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + (right_falcon.
-                                                                                       getSelectedSensorVelocity() * 10
-                                                                                       / 2048 * 60) * 8 / 1056) / 2
+    return ((falcon1.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + \
+           ((falcon2.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + \
+           ((falcon3.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + \
+           ((falcon4.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + \
+           ((falcon5.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) + \
+           ((falcon6.getSelectedSensorVelocity() * 10 / 2048 * 60) * 8 / 1056) / 6
 
 
 if __name__ == "__main__":
