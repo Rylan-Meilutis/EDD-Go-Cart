977cc43da68f435e5776b1a9eed3a3879a930564
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-03-29 20:33:28 -0400

    made it prettier

--- main.py
+++ main.py
@@ -15,16 +15,36 @@ falcon5 = TalonFX(5)
 falcon6 = TalonFX(6)
 
 
+def config() -> NoReturn:
+    falcon1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    falcon2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    falcon3.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    falcon4.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    falcon5.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    falcon6.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
+    brake_actuator.configSelectedFeedbackSensor(FeedbackDevice.Analog, 0)
+
+
+def run_motors(speed: float, control_mode=TalonFXControlMode.PercentOutput) -> NoReturn:
+    falcon1.set(control_mode, speed)
+    falcon2.set(control_mode, speed)
+    falcon3.set(control_mode, speed)
+    falcon4.set(control_mode, speed)
+    falcon5.set(control_mode, speed)
+    falcon6.set(control_mode, speed)
+
+
+def stop_motors(speed: float, control_mode=TalonFXControlMode.PercentOutput) -> NoReturn:
+    falcon1.set(control_mode, 0)
+    falcon2.set(control_mode, 0)
+    falcon3.set(control_mode, 0)
+    falcon4.set(control_mode, 0)
+    falcon5.set(control_mode, 0)
+    falcon6.set(control_mode, 0)
+
+
 def main() -> NoReturn:
     while True:
-        falcon1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        falcon2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        falcon3.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        falcon4.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        falcon5.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-        falcon6.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor)
-
-        brake_actuator.configSelectedFeedbackSensor(FeedbackDevice.Analog, 0)
         text = str(int(get_speed() + 0.5)) + " MPH"
         frame = np.zeros(shape=(1920, 1080, 3))
         cv2.putText(frame, text, (420, 800), cv2.FONT_HERSHEY_SIMPLEX, 1,
@@ -32,22 +52,10 @@ def main() -> NoReturn:
         cv2.imshow("Speedometer", frame)
         pot_pos = brake_actuator.getSelectedSensorPosition(0)
         if pot_pos > 10:
-            falcon1.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
-            falcon2.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
-            falcon3.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
-            falcon4.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
-            falcon5.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
-            falcon6.set(TalonFXControlMode.PercentOutput, pot_pos / pot_range)
+            run_motors(pot_pos / pot_range)
 
             brake_actuator.set(TalonSRXControlMode.PercentOutput, 0)
         elif pot_pos < -10:
-            falcon1.set(TalonFXControlMode.PercentOutput, 0)
-            falcon2.set(TalonFXControlMode.PercentOutput, 0)
-            falcon3.set(TalonFXControlMode.PercentOutput, 0)
-            falcon4.set(TalonFXControlMode.PercentOutput, 0)
-            falcon5.set(TalonFXControlMode.PercentOutput, 0)
-            falcon6.set(TalonFXControlMode.PercentOutput, 0)
-
             brake_actuator.set(TalonSRXControlMode.PercentOutput, pot_pos / pot_range)
 
 
