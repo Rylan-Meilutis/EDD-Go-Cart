ff4b2a0a1ef8af33b518df9d309a2e90267bd14c
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-03 11:06:52 -0400

    better testing logic

--- threading test.py
+++ threading test.py
@@ -6,6 +6,7 @@ from time import sleep
 def short_task():
     global q2
     while True:
+        sleep(2)
         q2 = random()
 
 
@@ -20,19 +21,23 @@ def main():
     while True:
         print(q1)
         print(q2)
+        sleep(1)
 
 
 if __name__ == "__main__":
-    q1 = 5
-    q2 = 3
-    t1 = Thread(target=long_task)
-    t2 = Thread(target=short_task)
-    t1.start()
-    t2.start()
-    main()
-    t1.join()
-    t2.join()
-
+    try:
+        q1 = 5
+        q2 = 3
+        t1 = Thread(target=long_task)
+        t2 = Thread(target=short_task)
+        t1.start()
+        t2.start()
+        main()
+        t1.join()
+        t2.join()
+    except KeyboardInterrupt:
+        print("exiting cuz of keyboard interrupt")
+        exit(2)
     # st = threading.Thread(target=short_task, args=("Thread-1", queue1))
     # lt = threading.Thread(target=long_task, args=("Thread-2", queue2))
     # m = threading.Thread(target=main, args=(queue1, queue2))
